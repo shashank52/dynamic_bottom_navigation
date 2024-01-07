88d24cc7328fc9b8d6e20e24da03face69f27aac
Shashank Agarwal <shashank.agarwal@elastic.run>
2024-01-07 13:56:11 +0530

    git init

---
+++ lib/screens/offers/view/offers_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OffersView extends StatelessWidget {
+  const OffersView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Offers'),
+    );
+  }
+}
